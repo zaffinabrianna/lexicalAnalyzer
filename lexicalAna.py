6f82777ee8c908d75e9e146976a3c3bcff004820
zaffinabrianna <zaffinabrianna@yahoo.com>
2025-03-10 22:49:16 -0700

    Finished token display info, need to write lines and understand better

--- lexicalAna.py
+++ lexicalAna.py
@@ -16,29 +16,50 @@ def removeUnnecessaryItems(lines):
 
         # Remove single-line comments
         for line in lines:
-            
+
             # Subs patterns it finds with ''
             line = re.sub(r'//.*', '', line)
 
             # Removes leading and trailing characters
             if line.strip(): 
-                clearedLines.appened(line.strip())
+                clearedLines.append(line.strip())
         return clearedLines
     
 def tokenization(lines):
     tokens = []
 
     #Map of lexemes and tokens using regex pattern
-    tokenIdentifier =
-    [
+    tokenIdentifier = [
         ('KEYWORD', r'\b(int|float|double|char|if|else|while|for|return)\b'), #Find pattern of keywords
         ('SEPARATOR', r'[(){};,]'), #Find pattern of seperators
-        ('IDENTIFIER', r'\b [a-z A-Z_]\w*\b'), #Find pattern of identifiers
+        ('IDENTIFIER', r'\b[a-z A-Z_]\w*\b'), #Find pattern of identifiers
         ('OPERATOR', r'[+\-*/=><!]'), #Find pattern of operators
         ('INTEGER', r'\b\d+\b'), #Find pattern of integers
         ('WHITESPACE', r'\s+'), #Find pattern of whitespace
         ('UNKOWN', r'.') #Catch unknown characters
     ]
 
+# Token that takes the the token identifier and puts it into one string
+    token_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in tokenIdentifier) 
+
+    for line in lines:
+        for match in re.finditer(token_regex, line):
+            token_type = match.lastgroup
+            token_value = match.group(token_type)
+
+            if token_type != 'WHITESPACE' and token_type != 'UNKNOWN':
+                tokens.append((token_value, token_type.lower()))
+    return tokens
 
 def main(): # Main to test lexical analysis
+    filename = input("Input file name: ")
+    lines = fileRead(filename)
+    cleanLines = removeUnnecessaryItems(lines)
+    tokens = tokenization(cleanLines)
+
+    print("Erm:\n")
+    for lexeme, token in tokens:
+         print(f'"{lexeme}" = {token}')
+
+if __name__ == "__main__":
+    main()
